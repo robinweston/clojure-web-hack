cb0f6027d7c1045b4bd4a35dc6fb36d4f623db34
Robin Weston <lcfcforever@hotmail.com>
2015-01-08 17:16:45 +0000

    Updated to new Compjure scaffolded project

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clojure-web-hack
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/clojure_web_hack/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-web-hack.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-web-hack.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
